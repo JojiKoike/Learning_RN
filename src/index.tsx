79f2a7b06f742a132cef444d1eb28cc4b71828f2
JojiKoike <joji.koike@gmail.com>
2021-03-28 14:14:02 +0900

    :+1: Stack

---
+++ src/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+
+const Main = () => {
+  const { navigate } = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Main</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigate('Sub');
+        }}
+      >
+        <Text>go to sub</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const Sub = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Sub</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="Sub" component={Sub} />
+    </Stack.Navigator>
+  );
+};
+
+const Index = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default Index;
